ea239d3308a06df0cc57efb66bf4b92fd46a2d46
ahlamboudali <hanane.boudali@gmail.com>
2024-06-04 14:54:07 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import UserContainer from "./components/UserContainer";
+import './App.css';
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Header title="Get User"/>
+      <UserContainer/>
+
+    </div>
+  );
+};
+
+
+export default App;
